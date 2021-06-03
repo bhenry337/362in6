4b05a9a9c8f72e4f81f34835efdbb3afde5ab625
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-06-02 20:58:18 -0700

    Added more tests

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,3 +5,9 @@ class TestCase(unittest.TestCase):
 	def test_div4(self):
 		self.assertEqual(leapyear.leapyear(2004), True)
 		pass
+	def test_div100(self):
+		self.assertEqual(leapyear.leapyear(2100), False)
+		pass
+	def test_div400(self):
+		self.assertEqual(leapyear.leapyear(2000), True)
+		pass
